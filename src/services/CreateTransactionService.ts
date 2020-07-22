c1b5ade3404d2a03a76ed6b651d9d470e8b753eb
BLoboF <brunofigueiredo_3@hotmail.com>
2020-07-22 17:40:39 -0300

    create-delete-fullTransaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,61 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_id: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Transaction Invalid. Choose "income" or "outcome".');
+    }
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError(
+        'Please check your acount you do not have enough money.',
+      );
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: { title: category_id },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = await categoriesRepository.create({
+        title: category_id,
+      });
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id: transactionCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
